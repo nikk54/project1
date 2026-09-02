3fcce45af42d9a178e0d3069566703ef33e09ecc
nikk54 <kulaevn45@gmail.com>
2026-09-02 22:34:25 +0300

    ask5

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
+#include<string.h>
 
 int main(void) {
-    int a[5];
-    for (int i = 0; i < 5; i++)scanf("%d\n", &a[i]);
-    int mx = a[0];
-    for (int i = 0; i < 5; i++) {
-        if (a[i] > mx)mx = a[i];
-    }
-    printf("%d\n", mx);
+    char input[256];
+    scanf("%255s", &input);
+    printf("%d", strlen(input));
 }
